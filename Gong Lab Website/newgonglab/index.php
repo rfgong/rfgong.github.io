51db72daa597de8fd4f9bc28abc597af573d09e6
Richard Gong <richardgong@Richards-MacBook-Pro-4.local>
2019-01-23 09:46:21 -0800

    Added and cleaned up content, removed excess files, draft complete

--- Gong Lab Website/newgonglab/index.php
+++ Gong Lab Website/newgonglab/index.php
@@ -17,6 +17,7 @@
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Raleway:200,400" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=News+Cycle|Rokkitt" rel="stylesheet">
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -32,86 +33,113 @@
 <div class="container">
   <div class="row" id="row1">
     <div class="col-lg-12 text-center" id="index">
-        <center><img width="70%" height="70%" src="static/img/logoT2.png" class="img-responsive" alt=""></center>
+        <center><img src="static/img/lablogo.png" class="img-responsive logo" alt=""></center>
     </div>
   </div>
   <div class="row">
-    <div class="box">
-      <div class="col-lg-12">
-        <!-- <hr>
-        <h2 class="intro-text text-center">Welcome to the Gong Lab! </h2>
-        <hr class="visible-xs"> -->
-      </div>
-    </div>
   </div>
   <div class="row">
     <div class="box">
       <div class="col-lg-12"> <a name="home"></a>
-        <h2 class="intro-text text-center">Text</h2>
-        <p>To investigate the underlying mechanisms of eye growth and disease development for understanding how the eyeball focuses the light and helps to form a clear image for the vision, by using experimental tools in genetics, molecular and cellular biology, biochemistry, and tissue engineering.</p>  
-        <p>Current research topics include:</p>
-        <ul>
-          <li>Lens development and cataract:  To study what functional roles played by intercellular gap junction channels, genetic factors and signaling molecules in the lens development and cataract formation. This project aims to elucidate new insights about how lens cell morphogenesis and homeostasis are maintained for the lens growth and transparency during development, and to provide innovative approaches for delaying age-related cataracts - a leading cause of blindness in the world.</li>
-          <li>Function and regulation of retinal pigment epithelium (RPE): To study how intracellular and intravesicular pH homeostasis is regulated by Na+/H+ exchanges (NHE) in RPE. The goal of this project is to understand how dysfunctional RPE, one of the key factors causing age-related macular degeneration (AMD), leads to retinal degeneration - the death of photoreceptors, and to develop novel methods for maintaining or regenerating RPE. AMD is a leading cause of blindness in the western world including the US.</li>
-          <li>Signal transduction in retinal and choroidal endothelial cells: To study the roles of surface receptors of low-density lipoprotein receptor families including Lrp5 and VLDLR in retinal angiogenesis. This project aims to elucidate novel mechanisms controlling vascular endothelial cells in development and diseases. </li>
-        </ul>
-        <hr>
+        <h2 class="intro-text text-center">RESEARCH</h2>
+        <br />
+        <div class="mission" id="transparent-white">
+          <p><strong>By using experimental tools in genetics, molecular and cellular biology, biochemistry, and tissue engineering, we investigate the underlying mechanisms of eye growth and disease development to understand how the eyeball focuses light and forms a clear image.</strong></p>  
+          <p><strong>Current research topics include:</strong></p>
+          <ul>
+            <li>Lens development and cataract formation:  To study what functional roles are played by intercellular gap junction channels, genetic factors, and signaling molecules in lens development and cataract formation. This project aims to elucidate new insights about how lens cell morphogenesis and homeostasis are maintained for lens growth and transparency during development, and to provide innovative approaches for delaying age-related cataracts - a leading cause of blindness in the world.</li>
+            <br>
+            <li>Function and regulation of retinal pigment epithelium (RPE):  To study how intracellular and intravesicular pH homeostasis is regulated by Na+/H+ exchanges (NHE) in RPE. The goal of this project is to understand how dysfunctional RPE, one of the key factors causing age-related macular degeneration (AMD), leads to retinal degeneration - the death of photoreceptors, and to develop novel methods for maintaining or regenerating RPE. AMD is a leading cause of blindness in the western world including the US.</li>
+            <br>
+            <li>Signal transduction in retinal and choroidal endothelial cells:  To study the roles of surface receptors of low-density lipoprotein receptor families including Lrp5 and VLDLR in retinal angiogenesis. This project aims to explore mechanisms controlling vascular endothelial cells in development and diseases.</li>
+          </ul>
+          <hr>
+        </div>
       </div>
     </div>
   </div>
   <div class="row">
     <div class="box">
       <div class="col-lg-12">
+        <h2 class="intro-text text-center">LAB MEMBERS</h2>
+      </div>
+      <div class="col-lg-12">
         <br>
       </div>
-      <div class="col-lg-12 text-center"> <img src="static/img/portfolio/holder.jpg" class="img-responsive center-block" alt="">
+      <div class="col-sm-1 col-sm-1 col-sm-1 text-center"></div>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center"> <img src="static/img/members/xiaGonS.jpg" class="img-responsive center-block" id="circular" alt="">
         <p class="member"><span class="name">Xiaohua Gong</span><br />
-          Professor<br />
+          PhD, Professor<br />
           xgong@berkeley.edu</p>
       </div>
-      <div class="col-lg-1 col-sm-1 col-xs-1 text-center"></div>
-      <div class="col-lg-5 col-sm-5 col-xs-5 text-center"> <img src="static/img/portfolio/holder.jpg" class="img-responsive center-block" alt="">
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center"> <img src="static/img/members/chuXiaS.jpg" class="img-responsive center-block" id="circular" alt="">
         <p class="member"><span class="name">Chun-Hong Xia</span><br />
-          Staff Scientist<br />
+          PhD, Associate Researcher<br />
           chxia@berkeley.edu</p>
       </div>
-      <div class="col-lg-5 col-sm-5 col-xs-5 text-center"> <img src="static/img/members/ML.jpg" class="img-responsive center-block" alt="">
+      <div class="col-sm-12 text-center"> <img src="static/img/members/meiLiS.jpg" class="img-responsive center-block" id="circular" alt="">
         <p class="member"><span class="name">Mei Li</span><br />
-          Staff Scientist<br />
+          PhD, MD, Gene Delivery Specialist<br />
           mei.li@berkeley.edu</p>
       </div>
-      <div class="col-lg-1 col-sm-1 col-xs-1 text-center"></div>
       <div class="col-lg-12">
         <br>
       </div>
-      <hr>
-        <h2 class="intro-text text-center">Graduate Researchers<br />
+        <h2 class="lower-header text-center">GRADUATE RESEARCHERS<br />
         </h2>
-      <hr>
       <div class="col-lg-12">
         <br>
       </div>
-      <div class="col-lg-1 col-sm-1 col-xs-1 text-center"></div>
-      <div class="col-lg-5 col-sm-5 col-xs-5 text-center"> <img src="static/img/members/JL.jpg" class="img-responsive center-block" alt="">
-        <p class="member"><span class="name">Jimmy Liu</span><br />
-          Postdoc<br />
-          chunyenliu@berkeley.edu</p>
+      <div class="col-sm-1 col-sm-1 col-sm-1 text-center"></div>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center"> <img src="static/img/members/guaZhaS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Guannan Zhao</span><br />
+          PhD, Postdoctoral Researcher<br /></p>
       </div>
-      <div class="col-lg-5 col-sm-5 col-xs-5 text-center"> <img src="static/img/portfolio/holder.jpg" class="img-responsive center-block" alt="">
-        <p class="member"><span class="name">Wiktor Stopka</span><br />
-          Grad Student<br />
-          wiktor.stopka@berkeley.edu</p>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center"> <img src="static/img/members/binKonS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Bin Kong</span><br />
+          PhD Student<br /></p>
       </div>
-      <div class="col-lg-1 col-sm-1 col-xs-1 text-center"></div>
+      <div class="col-sm-12 text-center"> <img src="static/img/members/yuxLiS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Yuxing Li</span><br />
+          PhD Student<br /></p>
+      </div>
+      <div class="col-lg-12">
+      </div>
+      <div class="col-sm-1 col-sm-1 col-sm-1 text-center"></div>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center">  <img src="static/img/members/racLiS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Rachel Li</span><br />
+          BS, Research Assistant<br /></p>
+      </div>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center"> <img src="static/img/members/sarChuS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Sarah Chu</span><br />
+          BS, Research Assistant<br /></p>
+      </div>
       <div class="col-lg-12">
         <br>
       </div>
-      <hr>
-        <h2 class="intro-text text-center">Undergraduate Researchers<br />
+        <h2 class="lower-header text-center">UNDERGRADUATE RESEARCHERS<br />
         </h2>
-      <hr>
       <div class="col-lg-12">
         <br>
+      </div>
+      <div class="col-sm-12 text-center"> <img src="static/img/members/wilHanS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">William Han</span><br />
+          Undergraduate Research Student<br /></p>
+      </div>
+      <div class="col-lg-12">
+      </div>
+      <div class="col-sm-1 col-sm-1 col-sm-1 text-center"></div>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center">  <img src="static/img/members/mirCheS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Miranda Chen</span><br />
+          Undergraduate Research Student<br /></p>
+      </div>
+      <div class="col-sm-5 col-sm-5 col-sm-5 text-center"> <img src="static/img/members/jesWanS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Jessica Wang</span><br />
+          Undergraduate Research Student<br /></p>
+      </div>
+      <div class="col-sm-12 text-center"> <img src="static/img/members/kimDuoS.jpg" class="img-responsive center-block" id="circular" alt="">
+        <p class="member"><span class="name">Kim Duong</span><br />
+          Undergraduate Research Student<br /></p>
       </div>
     </div>
   </div>
