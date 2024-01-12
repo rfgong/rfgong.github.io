90bb8f1485c2a2dfecb144c0ace280f081f59314
Richard Gong <richardgong@Richards-MacBook-Pro-2.local>
2024-01-12 10:20:55 -0800

    added one publication

--- Gong Lab Website/newgonglab/publications.php
+++ Gong Lab Website/newgonglab/publications.php
@@ -42,6 +42,8 @@
     <div class="box">
       <div class="col-lg-12">
         <ul class="publist">
+          <li><strong><a href="https://pubmed.ncbi.nlm.nih.gov/38137394/">"Identification and Characterization of Retinitis Pigmentosa in a Novel Mouse Model Caused by PDE6B-T592I."</a></strong><br />
+          <sup>Xia CH, Liu H, Li M, Zhang H, Xing X, Gong X</sup></li>
           <li><strong><a href="https://pubmed.ncbi.nlm.nih.gov/37306987/">"Adaptive Optical Two-Photon Fluorescence Microscopy Probes Cellular Organization of Ocular Lenses In Vivo."</a></strong><br />
           <sup>Paidi SK, Zhang Q, Yang Y, Xia CH, Ji N, Gong X</sup></li>
           <li><strong><a href="https://pubmed.ncbi.nlm.nih.gov/37039777/">"Retinal microvascular and neuronal pathologies probed in vivo by adaptive optical two-photon fluorescence microscopy."</a></strong><br />
